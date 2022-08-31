9affcb5f6d99006cb7177001cba0fb3e5eaad1e8
Leo <leo@wangrunlin.com>
2022-09-01 01:47:02 +0800

    style(webpack): fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,35 @@ const externals = require('externals-dependencies');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: './src/app.ts',
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)?$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { cacheDirectory: true },
-                },
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
+  mode: 'development',
+  entry: './src/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        use: {
+          loader: 'babel-loader',
+          options: { cacheDirectory: true },
+        },
+        exclude: /node_modules/,
+      },
     ],
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    target: 'node',
-    node: {
-        global: true,
-        __filename: true,
-        __dirname: true
-    },
-    externals: [externals()], // delete some warning log
-}
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  node: {
+    global: true,
+    __filename: true,
+    __dirname: true,
+  },
+  externals: [externals()], // delete some warning log
+};
