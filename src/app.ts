09ae57cae0390e543ad7da4040e0c65e658b4d11
Leo <leo@wangrunlin.com>
2022-09-01 01:43:09 +0800

    style(src): eslint app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Koa from 'koa';
 // const Koa = require('koa');
 
 const app = new Koa();
 
 // response
-app.use(ctx => {
+app.use((ctx) => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Application on listen http://127.0.0.1:3000');
+});
 
 export default app;
