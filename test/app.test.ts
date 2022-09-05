9c90b49978e7b43422c030483f059174670d11cb
Leo <leo@wangrunlin.com>
2022-09-05 23:22:57 +0800

    refactor(test): add module

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,38 +1,40 @@
-import { it, expect } from '@jest/globals';
+import { describe, it, expect } from '@jest/globals';
 import request from 'supertest';
 import app from '@src/app';
 
-it('Testing to see if Jest works', () => {
-  expect(1).toBe(1);
-});
+describe('Testing to see app is works', () => {
+  it('Testing to see if Jest works', () => {
+    expect(1).toBe(1);
+  });
 
-it('Hello World works', async () => {
-  const res = await request(app.callback()).get('/');
+  it('Hello World works', async () => {
+    const res = await request(app.callback()).get('/');
 
-  expect(res.status).toBe(200);
-  expect(res.text).toBe('Hello World');
-});
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('Hello World');
+  });
 
-// test
-it('test application is work', async () => {
-  const res = await request(app.callback()).get('/test');
+  // test
+  it('test application is work', async () => {
+    const res = await request(app.callback()).get('/test');
 
-  expect(res.status).toBe(200);
-  expect(res.text).toBe('ok');
-});
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('ok');
+  });
 
-it('test text', async () => {
-  const res = await request(app.callback()).get('/test/text');
+  it('test text', async () => {
+    const res = await request(app.callback()).get('/test/text');
 
-  expect(res.status).toBe(200);
-  expect(res.text).toBe('This is a text.');
-});
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('This is a text.');
+  });
 
-it('test json', async () => {
-  const res = await request(app.callback()).get('/test/json');
+  it('test json', async () => {
+    const res = await request(app.callback()).get('/test/json');
 
-  expect(res.status).toBe(200);
-  expect(res.body.code).toBe(200);
-  expect(res.body.success).toBe(true);
-  expect(res.body.message).toBe('ok');
+    expect(res.status).toBe(200);
+    expect(res.body.code).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(res.body.message).toBe('ok');
+  });
 });
