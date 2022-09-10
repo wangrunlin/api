aa7ece42bb33ba4d488dc406aa64cb855fc7b963
Leo <leo@wangrunlin.com>
2022-09-10 22:18:10 +0800

    fix(util): unsplash fetch not load

--- src/util/unsplash-js.ts
+++ src/util/unsplash-js.ts
@@ -1,11 +1,15 @@
 import { createApi } from 'unsplash-js';
 import { env } from 'process';
+import * as nodeFetch from 'node-fetch';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const accessKey: string = env.UNSPLASH_ACCESS_KEY ?? '';
-const unsplash = createApi({ accessKey });
+const unsplash = createApi({
+  accessKey,
+  fetch: nodeFetch.default as unknown as typeof fetch,
+});
 
 export default async function getRandomPhotos(): Promise<string> {
   const count = 1;
