d1c0d61b77e46b992459946f06a43fcf6d037fbc
Leo <leo@wangrunlin.com>
2022-09-22 23:20:15 +0800

    fix(random): cannot read properties of undefined (reading '0')
    - add default url

--- src/util/unsplash-js.ts
+++ src/util/unsplash-js.ts
@@ -11,6 +11,8 @@ const unsplash = createApi({
   fetch: nodeFetch.default as unknown as typeof fetch,
 });
 
+const defaultURL = 'https://images.unsplash.com/photo-1523730205978-59fd1b2965e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1054&q=80';
+
 export default async function getRandomPhotos(): Promise<string> {
   const count = 1;
   const random = await unsplash.photos.getRandom({
@@ -19,5 +21,5 @@ export default async function getRandomPhotos(): Promise<string> {
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  return random.response[0].urls.raw;
+  return random.response ? random.response[0].urls?.raw : defaultURL;
 }
